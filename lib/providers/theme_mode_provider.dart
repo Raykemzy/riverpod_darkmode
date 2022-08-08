fb9ccbf339eab939e228df562d03813f9354df42
Ukeme Raymond Ukofia <ukemzyukofia@gmail.com>
2022-08-08 15:51:44 +0100

    update

--- lib/providers/theme_mode_provider.dart
+++ lib/providers/theme_mode_provider.dart
@@ -5,9 +5,6 @@ final themeModeProvider =
 
 class ThemeNotifier extends StateNotifier<bool> {
   ThemeNotifier() : super(false);
-  final bool _isDark = false;
-  bool get isDark => _isDark;
-
   toggleTheme(bool value) {
     state = value;
   }
